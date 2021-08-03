4d54027f8d775d4a0b58bb8276780fcdbe67cc07
shiel1dm <shield55@msu.edu>
2021-08-03 13:34:03 -0400

    got a couple files commmunicating, starting to build framework of inquirer.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const inquirer = require('inquirer');
+const employee = require('./lib/employee.js');
+var staff = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the project managers name?'
+      
+    },
+    {
+      type: 'input',
+      name: 'Id',
+      message: 'What is the ID number'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the managers Email address: '
+    },
+    {
+      type: 'input',
+      name: 'office',
+      message: 'Please enter your office #'
+
+    },
+    {
+      type: 'checkbox',
+      message: 'Please select your role.',
+      name: 'role',
+      choices: ['Manager']
+
+    },
+  ])
+  .then((data) => {
+    console.log(data)
+    new employee(JSON.stringify(data))
+    console.log(staff)
+
+
+  })
+
