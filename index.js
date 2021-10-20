7e237994f7d5b1f83bfacac58983dc486ff61e32
shiel1dm <shield55@msu.edu>
2021-10-20 12:59:45 -0400

    starting to work again. updated lib files, added index.js. Either adding basic html next or starting to build functions in index.js

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 const inquirer = require('inquirer');
-const employee = require('./lib/employee.js');
-var staff = [];
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the project managers name?'
-      
-    },
-    {
-      type: 'input',
-      name: 'Id',
-      message: 'What is the ID number'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter the managers Email address: '
-    },
-    {
-      type: 'input',
-      name: 'office',
-      message: 'Please enter your office #'
+const Staff = require('./lib/employee.js').default;
+const manager = require('./lib/manager.js');
+//const Engineer = require('./lib/engineer.js')
+const intern = require('./lib/intern.js')
 
-    },
-    {
-      type: 'checkbox',
-      message: 'Please select your role.',
-      name: 'role',
-      choices: ['Manager']
 
-    },
-  ])
+
+new 
+inquirer.prompt(manager.firstQ)
   .then((data) => {
-    console.log(data)
-    new employee(JSON.stringify(data))
+    var name = data.name;
+    var id = data.id;
+    var email = data.email;
+    var offNum = data.office;
+    console.log(name, id, email, offNum)
+    
+    
+    
+    
+    //new manager.Manager(offNum)   
+    
+    inquirer.prompt(employee.newEmp)
+      .then((answer) =>{
+        const form = answer.continue[0];
+        console.log(form)
+        if(form === 'Engineer'){
+          inquirer.prompt(engineer.engineerQ)
+
+          .then((data) =>{
+            console.log(data)
+            console.log('In if for Engine')
+
+          })
+        }
+        else if(form === 'Intern'){
+          inquirer.prompt(intern.internQ)
+          .then((data) => {
+            console.log(data)
+            console.log('In else if for intern')
+          })
+        }
+        else{
+          console.log('end program')
+        }
+      })
+
     
   })
-
